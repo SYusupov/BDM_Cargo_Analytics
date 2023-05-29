e99ec689f129311f008fe4e2f46b69bbf0ebc6eb
xuyou1999 <yx1711@nyu.edu>
2023-05-29 17:29:42 +0200

    update schema

--- postgres_query/schema.sql
+++ postgres_query/schema.sql
@@ -1,29 +1,27 @@
 -- CREATE DATABASE formatted_zone;
 
-DROP TABLE IF EXISTS scheduledFlights;
-CREATE TABLE scheduledFlights (
-    id SERIAL PRIMARY KEY,
-    flightNumber VARCHAR(50),
-    departureAirportFsCode VARCHAR(50),
-    arrivalAirportFsCode VARCHAR(50),
-    departureTime TIMESTAMP,
-    arrivalTime TIMESTAMP,
-    stops INT,
-    departureTerminal INT,
-    arrivalTerminal INT
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+    flightNumber BIGINT,
+    departureAirportFsCode TEXT,
+    arrivalAirportFsCode TEXT,
+    departureTime TEXT,
+    arrivalTime TEXT,
+    stops BIGINT,
+    departureTerminal DOUBLE PRECISION,
+    arrivalTerminal DOUBLE PRECISION
 );
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    product_id VARCHAR(50) UNIQUE,
-    product_weight_g FLOAT,
-    product_length_cm FLOAT,
-    product_height_cm FLOAT,
-    product_width_cm FLOAT,
+    product_id TEXT UNIQUE,
+    product_weight_g DOUBLE PRECISION,
+    product_length_cm DOUBLE PRECISION,
+    product_height_cm DOUBLE PRECISION,
+    product_width_cm DOUBLE PRECISION,
     product_category_name_english TEXT,
-    product_name TEXT,
-    product_image_link TEXT
+    product_name TEXT
+--     product_image_link TEXT
 );
 
 DROP TABLE IF EXISTS addresses;
@@ -41,55 +39,72 @@ CREATE TABLE addresses (
 
 DROP TABLE IF EXISTS cities_distances;
 CREATE TABLE cities_distances (
-    id SERIAL PRIMARY KEY,
+    "Unnamed: 0" BIGINT,
     country1 TEXT,
-    latitude1 FLOAT,
-    longitude1 FLOAT,
     name1 TEXT,
     country2 TEXT,
-    latitude2 FLOAT,
-    longitude2 FLOAT,
     name2 TEXT,
-    distance_km FLOAT
+    distance_km DOUBLE PRECISION,
+    currency TEXT,
+    lpg_price DOUBLE PRECISION,
+    diesel_price DOUBLE PRECISION,
+    gasoline_price DOUBLE PRECISION
 );
+
 
 DROP TABLE IF EXISTS requests;
 CREATE TABLE requests (
-    id SERIAL PRIMARY KEY,
-    initializationUserId INT,
-    collectionUserId INT,
-    travellerId INT,
-    productId VARCHAR(50),
-    dateToDeliver TIMESTAMP,
-    dateDelivered TIMESTAMP,
-    requestDate TIMESTAMP,
-    pickUpAddress INT,
-    collectionAddress INT,
-    description TEXT,
-    deliveryFee FLOAT
+    initializationUserId BIGINT,
+    collectionUserId BIGINT,
+    travellerId DOUBLE PRECISION,
+    productId TEXT,
+    dateToDeliver TEXT,
+    dateDelivered TEXT,
+    requestDate TEXT,
+    pickUpAddress BIGINT,
+    collectionAddress BIGINT,
+    description DOUBLE PRECISION,
+    deliveryFee DOUBLE PRECISION
 );
 
 DROP TABLE IF EXISTS travels;
 CREATE TABLE travels (
-    id SERIAL PRIMARY KEY,
-    userId INT,
-    departureAirportFsCode VARCHAR(5),
-    arrivalAirportFsCode VARCHAR(5),
-    departureTime TIMESTAMP,
-    arrivalTime TIMESTAMP,
-    extraLuggage INT
+    userId BIGINT,
+    departureAirportFsCode TEXT,
+    arrivalAirportFsCode TEXT,
+    departureTime TEXT,
+    arrivalTime TEXT,
+    extraLuggage BIGINT
 );
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
-    firstname TEXT,
-    lastname TEXT,
+    user_id BIGINT UNIQUE,
+    -- firstname TEXT,
+    -- lastname TEXT,
     gender TEXT,
     nationality TEXT,
-    mobile VARCHAR(50),
-    dob DATE,
-    is_traveller BOOL,
-    address INT
+--     mobile VARCHAR(50),
+    dob TEXT,
+    is_traveller BOOLEAN,
+    address INT,
+    city TEXT
+);
+
+DROP TABLE IF EXISTS dhl_price;
+CREATE TABLE dhl_price (
+    Weight BIGINT,
+    A DOUBLE PRECISION,
+    B DOUBLE PRECISION,
+    C DOUBLE PRECISION,
+    D DOUBLE PRECISION
+);
+
+DROP TABLE IF EXISTS dhl_zone;
+CREATE TABLE dhl_zone (
+    Region_start TEXT,
+    Zone_start BIGINT,
+    Region_end TEXT,
+    Zone_end BIGINT,
+    Type TEXT
 );
